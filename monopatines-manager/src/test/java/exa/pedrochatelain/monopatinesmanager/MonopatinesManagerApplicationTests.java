f3c06735d67cda61d81593c4a59e1856f74123ae
pedrochatelain <pedrochatelain@gmail.com>
2023-10-31 16:40:00 -0300

    add monopatines-manager

---
+++ monopatines-manager/src/test/java/exa/pedrochatelain/monopatinesmanager/MonopatinesManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package exa.pedrochatelain.monopatinesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonopatinesManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
