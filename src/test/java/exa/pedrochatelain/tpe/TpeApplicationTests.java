15350355456bc61479a9ec075664362bab233a91
pedrochatelain <pedrochatelain@gmail.com>
2023-10-31 15:10:52 -0300

    first commit

---
+++ src/test/java/exa/pedrochatelain/tpe/TpeApplicationTests.java
@@ -0,0 +1,13 @@
+package exa.pedrochatelain.tpe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
