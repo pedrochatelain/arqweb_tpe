839e1a6700840eb193cb3e187fc8aefcfdcf7c3e
pedrochatelain <pedrochatelain@gmail.com>
2023-11-03 11:38:09 -0300

    add user-manager microservice

---
+++ user-manager/src/main/java/exa/usermanager/usermanager/UserManagerApplication.java
@@ -0,0 +1,13 @@
+package exa.usermanager.usermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagerApplication.class, args);
+    }
+
+}
